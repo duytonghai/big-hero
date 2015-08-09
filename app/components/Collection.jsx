809b19e3ae84b573a1e14c16c0de178523f23ddb
duytonghai <duy.tonghai@harveynash.vn>
2015-08-09 09:57:10 +0700

    Changed boardId

--- app/components/Collection.jsx
+++ app/components/Collection.jsx
@@ -15,12 +15,12 @@ var Collection = React.createClass({
 
 componentWillMount: function() {
   (function(d, s, id) {
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s); js.id = id;
-      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1044498398907793";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.4&appId=1044498398907793";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 },
 
   render: function() {
@@ -39,7 +39,7 @@ componentWillMount: function() {
             <h1>Big Hero</h1>
             <p>The oppotunity to improve yourself</p>
             <ul className="icons">
-              <li><div className="fb-share-button" data-href="http://big-hero.com:8000" data-layout="button"></div></li>
+              <li><div className="fb-share-button" data-href={window.location.href} data-layout="button"></div></li>
             </ul>
           </header>
 
